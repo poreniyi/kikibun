16ec68aa74abbd7ae00c8eb506c7e593d5ee201f
Poreniyi <poreniyi@gmail.com>
2020-04-20 16:56:04 -0400

    Added unqie validator

--- databaseApp/dataApp.js
+++ databaseApp/dataApp.js
@@ -1,23 +1,22 @@
 const mongoose=require('mongoose');
 const mongoDB='mongodb+srv://admin:specialpass@cluster0-2tj27.mongodb.net/test?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, {useNewUrlParser:true,useUnifiedTopology:true }).then(res => console.log('Connected to Database'));
+const unqieValidator=require('mongoose-unique-validator');
+mongoose.connect(mongoDB, {useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true }).then(res => console.log('Connected to Database'));
 const db=mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
+const assert = require('assert')
 let Schema=mongoose.Schema;
-var BookSchema=new Schema(
-    
-)
 
 let WordScehmaJLPT= new Schema({
-    name:{type:String,required:false},///
-    hiragana:{type:String,required:false},
-    hiragana:{type:String,required:false},
-    hiragana:{type:String,required:false},
+    name:{type:String,required:false, unique:true},
+    English:{type:String,required:false},
+    Kanji:{type:String,required:false},
+    Hiragana:{type:String,required:false},
     nLevel:{type:String, required:false, min:1, max:5},
     length:Number
     }
 );
+WordScehmaJLPT.plugin(unqieValidator);
 let WordScehmaGenki= new Schema({
         name:{type:String,required:false},///
         English:{type:String,required:false},
@@ -28,30 +27,30 @@ let WordScehmaGenki= new Schema({
         }
 )
 
-let N5=mongoose.model('N1',WordScehmaJLPT);
-let test2={
-    1:1,
-    2:1,
-    3:1,
-}
-let test=new N5({
-    name:"Test",
-    English:"Test field ENglish",
+let N1=mongoose.model('N1',WordScehmaJLPT);
+let N2=mongoose.model('N2',WordScehmaJLPT);
+
+let test=new N1({
+    name:"Test4",
+    English:"Test field English",
     Kanji:"Some kanji",
     Hiragana:"Some kana",
     nLevel:5,
     length:"20",
 });
-test.save(function (err){
-    if (err) return handlError(err);
-    else{
-        console.log("Item saved");
-    }
-});
+// test.save(function (err){
+//     if (err) return handlError(err);
+//     else{
+//         console.log("Item saved");
+//     }
+// });
 
 saveItem=(item)=>{
-    item.name=new N1(item);
-    item.save(function(err){
-        if(err) return handlError(err);
-    })
-}
+        N1(item).save(function(err){
+            if(err) console.log(err);
+            else{
+                console.log("Item saved");
+            }
+        })
+};
+saveItem(test);
