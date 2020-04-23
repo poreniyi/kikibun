32be057408d97117f818c9c288327f9b3d1420dc
Poreniyi <poreniyi@gmail.com>
2020-04-23 14:01:21 -0400

    added database query  for get route index

--- databaseApp/routes.js
+++ databaseApp/routes.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const router=express.Router();
 const N5=require("./dataApp");
+const database=require("./dataApp");
+const Genki=database.Genki;
+const JLPT=database.JLPT;
 
 router.get('/',function(req,res){
     res.sendFile(__dirname + "/index.html");
@@ -14,17 +17,23 @@ router.get("/r",function(req, res){
 });
 router.post('/results.html',function(req,res){
     console.log(req.body);
-    res.render('results',{data:req.body});
+    Genki.find({Kanji:req.body.text}).then(function(words){
+        console.log(words);
+        res.render('results',{data:words});
+    })
 });
 for(let i =1;i<6;i++){
     let path="/N";
     path+=i
     router.get(path,function(req,res){
         let view= path.substring(1);
-        N5.find({}).then(function(words){
+       JLPT.findOne({}).then(function(words){
             res.render(view,{data:words});
         });
     });
 };
 
 module.exports=router;
+// Genki.find({Kanji:'今'}).then(function(words){
+//    console.log(words); 
+// });
