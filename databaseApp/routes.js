bc8c9e9f4ddc9e4805e7b110d13a8d10df294897
Poreniyi <poreniyi@gmail.com>
2020-05-05 18:21:48 -0400

    removed all files to add .gitignore and reupload

--- databaseApp/routes.js
+++ databaseApp/routes.js
@@ -8,65 +8,56 @@ const JLPT=database.JLPT;
 router.get('/',function(req,res){
     res.sendFile(__dirname + "/index.html");
 });
-router.get('/results',function(req,res){
-    res.sendFile(__dirname + "/results.html")
-});
-
-router.get("/r",function(req, res){
-    res.render('results');
-});
 router.post('/results.html',async(req,res)=>{
         try{
+            let text=req.body.text;
+            console.log(req.body);
             let result = await myfunction(req.body.text);
             console.log("MY RESULT IS:" +result);
-            //result=[1,2,3];
-            res.render('results',{ data: result});
+            res.render('results',{ data: result, originalSentence:text});
         }
         catch(error){
             console.log(error);
         }
-        
 });
-for(let i =1;i<6;i++){
-    let path="/N";
-    path+=i
-    router.get(path,function(req,res){
-        let view= path.substring(1);
-       JLPT.findOne({}).then(function(words){
-            res.render(view,{data:words});
-        });
-    });
-};
 
-myfunction=  (string)=>{
+
+myfunction= async (string)=>{
     let wordArray=[];
     let index=0;
+    let result=[]
     const regex = /.+は.+です/gm;
     if (regex.test(string)){
-        let result=[]
         console.log("passed test");
        let split=string.split('は');
         split[0]=split[0];
         split[1]=split[1].slice(0,-2);
-        split.forEach( element=>{
-            Genki.find({Kanji:element}).then(function(words){
-                result[index]=words.toJSON();
-                index++;
-               words.forEach( value=>{              
-                   wordArray.push(value.toJSON());
-                   console.log("Length of word array is: "+wordArray.length)
-                   console.log("This is the index array"+result);
-               });
-           });
-       });   
+        const DB= await Genki.find({});
+        const data=DB.filter((item)=>{
+                return split.includes(item.Kanji);
+        })
+       return   data;
+    }else{;
+        return [];
     }
-   console.log("This is the word array:"+wordArray+"length"+wordArray.length);
-    //return [1,2,3];
-   // result=[...wordArray];
-   result=wordArray.slice();
-
-    console.log(result);
-    return result;
-    return wordArray;
 };
+
+
 module.exports=router;
+ 
+makeGenkiVocabRoutes= ()=>{
+    for(let i=1;i<23;i++){
+        let path="/Chapter:";
+        path+=i;
+        router.get(path, async(req, res)=>{
+        let data= await Genki.find({});
+        res.render('chapterViews',{words:data});
+        }); 
+    }
+}
+
+router.post('/Chapter:1',(req,res)=>{
+console.log(req.body);
+res.end();
+});
+makeGenkiVocabRoutes();
