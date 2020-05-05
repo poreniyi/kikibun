3cc295eac645328bd76d50c8fc90218ae731e811
Poreniyi <poreniyi@gmail.com>
2020-05-05 19:33:17 -0400

    Moved parsing functionality out of expressApp and into its own directory

---
+++ Parser/GenkiParser.js
@@ -0,0 +1,23 @@
+let Genki=require('../databaseApp/dataApp').Genki;
+Chapter1= async (string)=>{
+    let wordArray=[];
+    let index=0;
+    let result=[]
+    const regex = /.+は.+です/gm;
+    if (regex.test(string)){
+        console.log("passed test");
+       let split=string.split('は');
+        split[0]=split[0];
+        split[1]=split[1].slice(0,-2);
+        const DB= await Genki.find({});
+        const data=DB.filter((item)=>{
+                return split.includes(item.Kanji);
+        })
+       return   data;
+    }else{;
+        return [];
+    }
+};
+module.exports={
+    Chapter1:Chapter1,
+};
