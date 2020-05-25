1c9d2285e9e48f38fbdd76ad29d4bb17cd526914
Poreniyi <poreniyi@gmail.com>
2020-05-25 14:47:40 -0400

    updated GenkiParser

--- Parser/GenkiParser.js
+++ Parser/GenkiParser.js
@@ -1,4 +1,7 @@
 let Genki=require('../databaseApp/dataApp').Genki;
+let dbScripts=require('../databaseApp/databaseScripts');
+let tokenizer=require('../kuromojinParser/Tokenizer');
+
 Chapter1= async (string)=>{
     let wordArray=[];
     let index=0;
@@ -17,7 +20,22 @@ Chapter1= async (string)=>{
     }else{;
         return [];
     }
+    
 };
+
+ Chapter1Update=async(string)=>{
+   let results=await tokenizer.tokenize(string);
+   results.forEach(async element => {
+        element.status= await dbScripts.documentExists(element.text) ? "Known" :"Unknown"
+        console.log(element.status);
+   });
+   //console.log(element.pos);
+   return results;
+ } 
+   
+
+
 module.exports={
     Chapter1:Chapter1,
+    Chapter1Update,Chapter1Update,
 };
