9587c8f9b9514b7c8bcd71e7c284fad24b76c942
Poreniyi <poreniyi@gmail.com>
2020-07-03 13:00:13 -0400

    added eventlistners for buttons to toggle colors of POS on click

--- assets/Scripts/Results3.js
+++ assets/Scripts/Results3.js
@@ -1,11 +1,51 @@
 let buttons=[...document.querySelectorAll('button')];
-let VerbColor=getComputedStyle(document.body).getPropertyValue('--Verb-Color');
-let VerbColor=getComputedStyle(document.body).getPropertyValue('--Noun-Color');
-let VerbColor=getComputedStyle(document.body).getPropertyValue('--Particle-Color');
-let VerbColor=getComputedStyle(document.body).getPropertyValue('--Na-Adjective-Color');
-let VerbColor=getComputedStyle(document.body).getPropertyValue('--I-Adjective-Color');
+let verbColor=getComputedStyle(document.body).getPropertyValue('--Verb-Color');
+let nounColor=getComputedStyle(document.body).getPropertyValue('--Noun-Color');
+let particleColor=getComputedStyle(document.body).getPropertyValue('--Particle-Color');
+let naAdjectiveColor=getComputedStyle(document.body).getPropertyValue('--Na-Adjective-Color');
+let iAdjectiveColor=getComputedStyle(document.body).getPropertyValue('--I-Adjective-Color');
 
-console.log(`Verb color is: ${VerbColor}`);
-buttons.forEach(button=>{
-    console.log(`The value of this button is:${button.value}`);
+let verbs=document.querySelectorAll('.Verb');
+let nouns=document.querySelectorAll('.Noun');
+let particles=document.querySelectorAll('.Particle');
+let naAdjectives=document.querySelectorAll('.Na-Adjective');
+let iAdjectives=document.querySelectorAll('.I-Adjective');
+let words=[verbs,nouns,particles,naAdjectives,iAdjectives];
+let pos={
+    "Nouns":nouns,
+    "Verbs":verbs,
+    "Particles":particles,
+    "I-Adjective":iAdjectives,
+    "Na-Adjective":naAdjectives,
+}
+console.log(`Number of pos is: ${words.length}`);
+words.forEach(word=>{
+    console.log(`I am ${word.length} words long`);
 })
+
+let colors={
+    "Nouns":nounColor,
+    "Verbs":verbColor,
+    "Particles":particleColor,
+    "I-Adjective":iAdjectiveColor,
+    "Na-Adjective":naAdjectiveColor,
+}
+let initalColor=false;
+
+buttons.forEach(button=>{
+    button.addEventListener('click',()=>{
+       let operatedArray=pos[button.value] ;
+       let operatedArrayColor=colors[button.value];
+        
+       operatedArray.forEach(element=>{
+           if (element.style.color=='') element.style.color=operatedArrayColor;
+           else element.style.color='';
+       })
+    })
+})
+let change={
+
+}
+let changeColors=function(acolor){
+
+}
