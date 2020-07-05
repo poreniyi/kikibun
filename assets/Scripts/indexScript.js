8d70a639c9e51ba304bd7eb91b3b5ada57e3358b
Poreniyi <poreniyi@gmail.com>
2020-07-05 13:27:16 -0400

    made submit button visible by default

--- assets/Scripts/indexScript.js
+++ assets/Scripts/indexScript.js
@@ -10,7 +10,6 @@ let inputs=[jlpt,genki];
 console.log(`JLPT text is ${jlpt.value}`);
 console.log(`Genki text is ${genki.value}`);
 console.log(`Submit button value is ${button.value}`);
-button.style.visibility='hidden';
 inputs.forEach(element=>{
     element.addEventListener("change",()=>{
         if(jlpt.value!=0||genki.value!=0){
@@ -19,7 +18,6 @@ inputs.forEach(element=>{
             button.style.visibility='hidden';
         }
         console.log(`Change test ${element.value}`);
-    })
-    
+    })   
 })
 console.log("am I working");
