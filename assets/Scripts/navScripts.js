9daf4e1479fb96ee533baeb9f73ca915de256a5d
Poreniyi <poreniyi@gmail.com>
2020-07-13 00:15:54 -0400

    added active class on a instead of li element

--- assets/Scripts/navScripts.js
+++ assets/Scripts/navScripts.js
@@ -1,13 +1,15 @@
 let list=[...document.querySelector('.ul').getElementsByTagName("li")];
-//let list=ul.getElementsByTagName("li");
-console.log(list[0].firstChild.href);
+console.log(`List length is ${list.length}`)
 list.forEach(element=>{
     let href=element.firstChild.getAttribute('href');
     let pathname=window.location.pathname+window.location.search;
     let url=window.location.href
    // console.log(`The href is:${element.getAttribute("a href")}`);
    if(href==pathname || href == url){
-       element.className="active";
+      let link= element.querySelector('a');
+       link.className="active";
+   }else{
+       console.log(`else ${href} url is ${url}`);
    }
 });
 
