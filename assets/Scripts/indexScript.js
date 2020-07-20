ac1eeb3e6a191e15b13bad9de6c8dfe63b25fb68
Poreniyi <poreniyi@gmail.com>
2020-07-20 14:59:12 -0400

    added initial values for genki and JLPT and made button hidden by default

--- assets/Scripts/indexScript.js
+++ assets/Scripts/indexScript.js
@@ -1,15 +1,12 @@
 
 const form=document.querySelector('#form')
 const button= document.querySelector("#button")
-const ap= document.querySelector("#ap")
 
-const textArea=document.querySelector("#text");
 const genki=document.querySelector("#genki");
 const jlpt=document.querySelector("#JLPT");
 let inputs=[jlpt,genki];
-console.log(`JLPT text is ${jlpt.value}`);
-console.log(`Genki text is ${genki.value}`);
-console.log(`Submit button value is ${button.value}`);
+button.style.visibility='hidden';
+
 inputs.forEach(element=>{
     element.addEventListener("change",()=>{
         if(jlpt.value!=0||genki.value!=0){
@@ -20,4 +17,3 @@ inputs.forEach(element=>{
         console.log(`Change test ${element.value}`);
     })   
 })
-console.log("am I working");
