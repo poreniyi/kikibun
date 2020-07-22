edd476269ee5e9f95d4b3fd2a0b5d54f8284e8b0
Poreniyi <poreniyi@gmail.com>
2020-07-21 22:17:34 -0400

    added secret has env variable and session as a use of app

--- expressApp/index.js
+++ expressApp/index.js
@@ -1,17 +1,26 @@
 var express=require('express');
 let app=express();
 const mongoose=require('mongoose');
+const cookieParser = require('cookie-parser')
+const session=require('express-session');
+
 require('dotenv').config();
 path = require('path')
 
 let bodyParser=require('body-parser');
 const mongoDB=process.env.URL;
-mongoose.connect(mongoDB, {useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true }).then(res => console.log('Connected to Database'));
+mongoose.connect(mongoDB, {useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true }).then(
+    res => console.log('Connected to Database'));
 const db=mongoose.connection;
  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-
+let sessionSecret=process.env.Secret;
+let sess={
+    secret:process.env.Secret,
+    resave:true,
+    saveUninitialized: true,
+}
 const  routes=require("./routes");
+app.use(session(sess));
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, './views' ));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,7 +29,6 @@ app.use('/assets',express.static(path.join(__dirname, '../assets')));
 app.use(function(err,req,res, next){
     res.status(422).send({ error: err.message});
 })
-
 app.listen(process.env.PORT||3000   ,function(){
     let port=(process.env.PORT || 3000);
     console.log(`now listening for requests on port ${port}`);
