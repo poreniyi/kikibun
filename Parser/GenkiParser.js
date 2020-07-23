e1a3ba7a228e4092c8b7ea0cdd3917af9c64849b
Poreniyi <poreniyi@gmail.com>
2020-07-23 18:59:37 -0400

    removed old commented out DB queries and uncessary console.logs also Improved efficency by removing double queries and putting conditional on the data itself

--- Parser/GenkiParser.js
+++ Parser/GenkiParser.js
@@ -1,42 +1,12 @@
 let Genki=require('../databaseApp/dataApp').Genki;
 let Particles=require('../databaseApp/dataApp').Particles;
+let JLPT=require('../databaseApp/dataApp').JLPT;
 
 let dbScripts=require('../databaseApp/databaseScripts');
 const particleFound=dbScripts.particleExists;
 let tokenizer=require('../kuromojinParser/Tokenizer');
 
-Chapter1= async (string)=>{
-    let wordArray=[];
-    let index=0;
-    let result=[]
-    const regex = /.+は.+です/gm;
-    if (regex.test(string)){
-        console.log("passed test");
-       let split=string.split('は');
-        split[0]=split[0];
-        split[1]=split[1].slice(0,-2);
-        const DB= await Genki.find({});
-        const data=DB.filter((item)=>{
-                return split.includes(item.Kanji);
-        })
-       return   data;
-    }else{;
-        return [];
-    }
-    
-};
-
- Chapter1Update=async(string)=>{
-   let results=await tokenizer.tokenize(string);
-   results.forEach(async element => {
-        element.status= await dbScripts.documentExists(element.text) ? "Known" :"Unknown"
-        console.log(element.status);
-   });
-   //console.log(element.pos);
-   return results;
- } 
-   
-GenkiParser=async (tokens,chapter)=>{
+GenkiParser=async (tokens,chapter,nLVL)=>{
     let wordKnownCounter=0;
     let wordStats={};
     let totalWords=tokens.length;
@@ -51,29 +21,21 @@ GenkiParser=async (tokens,chapter)=>{
         let afterPart=singleToken.conjugatedParts;
         let beforePart=singleToken.before;
         let base=singleToken.base;
-        await findParticles(afterPart,chapter,singleToken,'Form');
-        await findParticles(beforePart,chapter,singleToken,'Before');   
-        singleToken.statusKnown= await 
+        await findParticles(afterPart,chapter,nLVL,singleToken,'Form');
+        await findParticles(beforePart,chapter,nLVL,singleToken,'Before');   
+        let word= await 
         Genki.findOne({$or:[
             {Kanji:base,Chapter:{$lte:chapter}},
             {Chapter:{$lte:chapter},Hiragana:base, Kanji:"none"}
-        ]}).lean()? true: false;
-        if(singleToken.statusKnown){
+        ]}).select('Chapter English').lean();
+        if(word){
+            singleToken.statusKnown=true;
             wordKnownCounter++;
-          //  singleToken.Chapter=await  Genki.findOne({Kanji:base}).select(' -_id Chapter'));
-            //singleToken.Chapter= await Genki.findOne({Kanji:base}).Chapter;
-            let word=await 
-            Genki.findOne({$or:[
-                {Kanji:base,Chapter:{$lte:chapter}},
-                {Chapter:{$lte:chapter},Hiragana:base, Kanji:"none"}
-            ]},'Chapter English').lean();
             singleToken.Chapter=word.Chapter;
             singleToken.addDescription(word.English);
         }
        
     }
-   // let wordKnownPercentage=+(wordKnownCounter/tokens.length*100).toFixed(2);
-    let wordKnownPercentage=wordKnownCounter;
     wordStats.total=totalWords;
     wordStats.known=wordKnownCounter;
 
@@ -82,24 +44,19 @@ GenkiParser=async (tokens,chapter)=>{
             stats:wordStats,
         };
 }
-let findParticles=async(array,chapter,word,position)=>{
+let findParticles=async(array,chapter,nLVL,word,position)=>{
     for(let j=0 ; j<array.length; j++){
         let element=array[j];
         let text=array[j].text;
         let pos=word.EnPOS.toLowerCase();
         let test='Chapter';
-       // let data=await Particles.findOne({Form:text,Chapter:{$lte:chapter}}) ? true: false;
         let data=await Particles.findOne({
             [position]:text,Chapter:{$lte:chapter},POSActedOn:{$in:[pos,'All']}
-             }).lean() ? true: false;
-
+             }).select('Name Chapter').lean();
         if(data){
-            let particle=await Particles.findOne({
-                [position]:text,Chapter:{$lte:chapter},POSActedOn:{$in:[pos,'All']}
-                 },'Name Chapter').lean();
             element.makeKnown();
-            element.updateDescription(particle.Name);
-            element.updateChapter(particle.Chapter);
+            element.updateDescription(data.Name);
+            element.updateChapter(data.Chapter);
             word.updateNumberBeforeKnown();
             //console.log(`Success ${text} has been found of ${counter.base}`);
            // console.log(`The counter is now:${counter}`);
@@ -110,7 +67,5 @@ let findParticles=async(array,chapter,word,position)=>{
  }
 
 module.exports={
-    Chapter1:Chapter1,
-    Chapter1Update,Chapter1Update,
     GenkiParser:GenkiParser,
 };
