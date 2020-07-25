d01b0865689c50b5afa1611313c1ba311267567c
Poreniyi <poreniyi@gmail.com>
2020-07-24 20:18:56 -0400

    commented out hiding submit button by default

--- assets/Scripts/indexScript.js
+++ assets/Scripts/indexScript.js
@@ -5,9 +5,11 @@ const button= document.querySelector("#button")
 const genki=document.querySelector("#genki");
 const jlpt=document.querySelector("#JLPT");
 let inputs=[jlpt,genki];
-if(!jlpt.value&&!genki.value){
-    button.style.visibility='hidden';
-}
+// if(!jlpt.value&&!genki.value){
+//     button.style.visibility='hidden';
+// }
+console.log(button);
+
 inputs.forEach(element=>{
     element.addEventListener("change",()=>{
         if((jlpt.value!=0||genki.value!=0)){
