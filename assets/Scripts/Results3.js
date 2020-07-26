f5181ffbfb71d01bd47ebe6bb5ef32d1065e9f04
Poreniyi <poreniyi@gmail.com>
2020-07-25 23:36:27 -0400

    added modal

--- assets/Scripts/Results3.js
+++ assets/Scripts/Results3.js
@@ -204,3 +204,28 @@ console.log(`Total # of known conjugations is ${knownConjugations}`);
 let conjugationStats=document.getElementById('ConjugationStats');
 
 conjugationStats.textContent=`${knownConjugations}/${totalConjugations.length} or ${+(knownConjugations/totalConjugations.length*100).toFixed(2)}% of all conjugations`;
+
+//MODAL
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
