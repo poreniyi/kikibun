b3da70b8c51856e6c3e62e83c9097b10003d6e44
Poreniyi <poreniyi@gmail.com>
2020-07-27 16:21:55 -0400

    added a animation for loading results

--- assets/Scripts/indexScript.js
+++ assets/Scripts/indexScript.js
@@ -2,6 +2,7 @@
 const form=document.querySelector('#form')
 const button= document.querySelector("#button")
 
+const loadScreen=document.querySelector('#loadWait');
 const genki=document.querySelector("#genki");
 const jlpt=document.querySelector("#JLPT");
 let inputs=[jlpt,genki];
@@ -9,7 +10,12 @@ let inputs=[jlpt,genki];
 //     button.style.visibility='hidden';
 // }
 console.log(button);
+button.addEventListener('click',()=>{
+    if(jlpt.value==0&&genki.value==0){
 
+    }
+    loadScreen.textContent='Getting Results Please wait';
+})
 inputs.forEach(element=>{
     element.addEventListener("change",()=>{
         if((jlpt.value!=0||genki.value!=0)){
